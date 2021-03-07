6d56183eea9498642ff0f2604464d82f5e933b58
omron <omron@omron-desktop>
2021-03-07 22:34:38 +0000

    build

--- main.c
+++ main.c
@@ -1,47 +1,28 @@
-/******************************************************************************/
-/* Files to Include                                                           */
-/******************************************************************************/
-
-#if defined(__XC)
-    #include <xc.h>         /* XC8 General Include File */
-#elif defined(HI_TECH_C)
-    #include <htc.h>        /* HiTech General Include File */
-#endif
-
 #include <stdint.h>        /* For uint8_t definition */
 #include <stdbool.h>       /* For true/false definition */
+#include <xc.h>         /* XC8 General Include File */
 
 
+#include "cristal.h"
+#include "i2c.h"
+#include "simon_hd44780_lcd.h"
+#include "pic16f877a.h"
 #include "system.h"        /* System funct/params, like osc/peripheral config */
 #include "user.h"          /* User funct/params, such as InitApp */
-#include "i2c.h"
-#include "lcd16.h"
-#include "pic16f877a.h"
 
 
-/******************************************************************************/
-/* User Global Variable Declaration                                           */
-/******************************************************************************/
 
-/* i.e. uint8_t <variable_name>; */
-
-/******************************************************************************/
-/* Main Program                                                               */
-/******************************************************************************/
 void main(void)
 {
-    /* Configure the oscillator for the device */
-    configure_osc();
-
-    /* Initialize I/O and Peripherals for application */
     init_app();
-    init_i2c();
-    init_lcd16();
 
-    while(1)
-    {
-        
-        /* TODO <INSERT USER APPLICATION CODE HERE> */
+    lcd_print("Hello World");
+    
+    while (1) {
+    PORTBbits.RB1 = 1;
+    __delay_ms(500);
+    PORTBbits.RB1 = 0;
+    __delay_ms(500);
     }
 
 }
