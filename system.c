3ac91ff464095e95eab6e0e4dc669e6e8ec5ab59
omron <omron@omron-desktop>
2021-03-07 22:34:52 +0000

    build

--- system.c
+++ system.c
@@ -16,32 +16,23 @@
 
 /* Refer to the device datasheet for information about available
 oscillator configurations. */
-void configure_osc(void)
-{
-    /* Typical actions in this function are to tweak the oscillator tuning
-    register, select new clock sources, and to wait until new clock sources
-    are stable before resuming execution of the main project. */
-    
-    /* Clock Source */
-    OPTION_REGbits.T0CS = 1; // transition on RA4/T0CKI pin
-    OPTION_REGbits.T0SE = 1; // increment on high to low
-    
-    /* Prescaler Rate Select bits   */
-    /* 1 : 128                      */
-    OPTION_REGbits.PS0 = 0;
-    OPTION_REGbits.PS1 = 1;
-    OPTION_REGbits.PS2 = 1;
-           
-}
-
-
-void configure_interrupts(void) 
+void configure_interrupts(void)
 {
     /* Enables TMR1 Overflow Interrupt 
      * and Synchronous Serial Port Interrupt Enable bit
      */
     PIE1 = 0x09; 
+    //PIE1 = 0x00;
     //PIR1
     //PIE2
     //PIR2
+
+           
+}
+
+
+static void enable_global_int(void)
+{
+    INTCONbits.GIE = 1;
+    INTCONbits.PEIE = 1;
 }
