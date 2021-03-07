23a181bd6010636c4d3a7eeadc9ed282ca0a6b99
omron <omron@omron-desktop>
2021-03-07 22:35:20 +0000

    build

---
+++ pcf84naoseiquantos.h
@@ -0,0 +1,17 @@
+
+/* 
+ * File:   pcf84naoseiquantos.h
+ * Author: William da Conceiçao
+ * Comments: divide and conquer
+ * Created: today 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef PCF_H
+#define	PCF_H
+#define SLAVE_ADDR_R 0x28
+#define SLAVE_ADDR_W 0x4E
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
