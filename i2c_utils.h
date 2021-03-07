7b54e7c2c1aee1e30a27373d2e680c91e8ccdf30
omron <omron@omron-desktop>
2021-03-07 22:35:16 +0000

    build

---
+++ i2c_utils.h
@@ -0,0 +1,22 @@
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef XC_HEADER_TEMPLATE_H
+#define	XC_HEADER_TEMPLATE_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+void master_w8(void);
+void wait_mssp(void);
+void check_bf_i2c(void);
+void check_ack_bit_i2c(void);
+void error_i2c(void);
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
